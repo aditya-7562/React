d98d7a2f14aa64d760d9f9e9bede073fe1ffc69e
aditya-7562 <aditya.12114424@lpu.in>
2024-08-16 13:05:02 +0530

    sduncls

--- first.js
+++ first.js
@@ -1,22 +1,33 @@
 // variables
 
-var name = "kalam";
-console.log(name);
+// var name = "kalam";
+// console.log(name);
 
-var age = 83;
-console.log(age);
+// var age = 83;
+// console.log(age);
 
-let place = "punjab";
-console.log(place);
+// let place = "punjab";
+// console.log(place);
 
 // function display(userName, userAge){
 //     return 'My inspiration name is ' + userName + '\n' + 'the age is ' + userAge + ' ';
 // }
 // console.log(display(name,age));
 
-function display(userName, userAge){
-    return 'My inspiration name is ' + name + '\n' + 'the age is ' + age + ' ';
-}
-console.log(display());
+// function display(userName, userAge){
+//     return 'My inspiration name is ' + name + '\n' + 'the age is ' + age + ' ';
+// }
+// console.log(display());
 
 // console.log("hey there");
+
+var num = 50;
+
+function add(){
+    var sum = num * 10;
+    console.log(sum);
+    console.log(num);
+}
+add();
+console.log(num);
+// console.log(sum);
