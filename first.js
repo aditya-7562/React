8bffd25cfed640becce178413dbb74d8a983e78c
aditya-7562 <aditya.12114424@lpu.in>
2024-08-16 12:47:01 +0530

    sdunclkds

--- first.js
+++ first.js
@@ -9,9 +9,14 @@ console.log(age);
 let place = "punjab";
 console.log(place);
 
-function display(userName, userAge){
-    return 'My inspiration name is ' + userName + '\n' + 'the age is ' + userAge + ' ';
-}
-console.log(display(name,age));
+// function display(userName, userAge){
+//     return 'My inspiration name is ' + userName + '\n' + 'the age is ' + userAge + ' ';
+// }
+// console.log(display(name,age));
 
-console.log("hey there");
+function display(userName, userAge){
+    return 'My inspiration name is ' + name + '\n' + 'the age is ' + age + ' ';
+}
+console.log(display());
+
+// console.log("hey there");
