9526ca5d9621e29c8836702b82973e199d52cab0
aditya-7562 <aditya.12114424@lpu.in>
2024-08-16 12:39:10 +0530

    add

--- first.js
+++ first.js
@@ -8,3 +8,9 @@ console.log(age);
 
 let place = "punjab";
 console.log(place);
+
+function display(userName, userAge){
+    return 'My inspiration name is ' + userName + '\n' + 'the age is ' + userAge + ' ';
+}
+console.log(display(name,age));
+
