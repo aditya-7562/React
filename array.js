d7016dad66f44721a5639dc95a460633900096e8
aditya-7562 <aditya.12114424@lpu.in>
2024-08-23 12:47:46 +0530

    ashdhn

--- array.js
+++ array.js
@@ -1,6 +1,15 @@
-const students = ['Aditya', 'Arpit', 'Vikas'];
-console.log(students);
+// const students = ['Aditya', 'Arpit', 'Vikas'];
+// console.log(students);
 
-for (const i of students) {
-    console.log(i);
-}
+// for (const i of students) {
+//     console.log('Name: ' + i);
+// }
+
+// console.log(students.map(students => 'Name: ' + students));
+
+
+const item1 = ['idly', 'dosa'];
+const item2 = item1.slice();
+item1.push('sambar');
+console.log(item1);
+console.log(item2);
