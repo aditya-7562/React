a350845df84463b2613d93575bef580181358578
aditya-7562 <aditya.12114424@lpu.in>
2024-08-23 12:55:19 +0530

    ashdhn

--- array.js
+++ array.js
@@ -8,8 +8,11 @@
 // console.log(students.map(students => 'Name: ' + students));
 
 
-const item1 = ['idly', 'dosa'];
-const item2 = item1.slice();
-item1.push('sambar');
-console.log(item1);
-console.log(item2);
+// const item1 = ['idly', 'dosa'];
+// item1.push('sambar');
+// const item2 = item1.slice();
+// item1.push('Chapati');
+// console.log(item1);
+// console.log(item2);
+
+
