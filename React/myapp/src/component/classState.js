06269fc4bb93435ba92749a570ee3d529f9a7e09
aditya-7562 <aditya.12114424@lpu.in>
2024-09-13 13:49:32 +0530

    activity

--- React/myapp/src/component/classState.js
+++ React/myapp/src/component/classState.js
@@ -9,7 +9,7 @@ export default class State extends Component {
     return (
       <div>
         <h1 style = {{color: this.state.color}}>My car color is {this.state.color}</h1>
-        <button onClick={() => {this.setState({color: "red"})}}>Change color</button>
+        <button onClick={() => {this.setState({color: "yellow"})}}>Change color</button>
       </div>
     )
   }
